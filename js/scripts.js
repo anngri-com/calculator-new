541ccdf1c65b4e6ec89065fe4098ab26b550964f
Anna and Rochelle <rochelle@>
2019-06-05 10:42:13 -0700

    added booking succesful to the page

--- js/scripts.js
+++ js/scripts.js
@@ -16,6 +16,10 @@ var divide = function(number1, number2) {
   return number1 / number2;
 };
 
+var comment = function(){
+  return "Booking Succesful";
+};
+
 // User interface (or front-end) logic:
 
 $(document).ready(function(){
@@ -32,6 +36,16 @@ $(document).ready(function(){
     var number1 = parseInt($("#subtract1").val());
     var number2 = parseInt($("#subtract2").val());
     var result = subtract(number1, number2);
+    $("#output").text(result);
+  });
+
+  // comment
+
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#subtract1").val());
+    var number2 = parseInt($("#subtract2").val());
+    var result = comment();
     $("#output").text(result);
   });
 });
